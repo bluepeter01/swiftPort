bbc67e045476af2dd40d355fa7151c6568c07ddf
Josiah <carmenjosh84@gmail.com>
2025-10-17 13:26:36 +0100

    Fix Netlify build adapter

--- svelte.config.js
+++ svelte.config.js
@@ -4,13 +4,8 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-
   kit: {
-    adapter: adapter({
-      // Optional Netlify options:
-      edge: false, // set true for Edge Functions
-      split: false // set true if you want separate serverless functions
-    })
+    adapter: adapter(),
   }
 };
 
